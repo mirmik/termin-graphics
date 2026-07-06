6edc121af4c0ec2c6e08f96f1f96faf0f552e2f1
mirmik <mirmikns@yandex.ru>
2026-07-06 14:43:44 +0300

    Add LINE\_TUBE\_BODY and LINE\_TUBE\_CAP shader variant ops to Python bindings. Update tests to verify that shader usage collection correctly identifies these new variant operations.

--- termin-graphics/python/bindings/shader_bindings.cpp
+++ termin-graphics/python/bindings/shader_bindings.cpp
@@ -122,7 +122,9 @@ void bind_shader(nb::module_& m) {
         .value("MORPHING", TC_SHADER_VARIANT_MORPHING)
         .value("FOLIAGE", TC_SHADER_VARIANT_FOLIAGE)
         .value("FOLIAGE_SHADOW", TC_SHADER_VARIANT_FOLIAGE_SHADOW)
-        .value("LINE_MATERIAL_FRAGMENT", TC_SHADER_VARIANT_LINE_MATERIAL_FRAGMENT);
+        .value("LINE_MATERIAL_FRAGMENT", TC_SHADER_VARIANT_LINE_MATERIAL_FRAGMENT)
+        .value("LINE_TUBE_BODY", TC_SHADER_VARIANT_LINE_TUBE_BODY)
+        .value("LINE_TUBE_CAP", TC_SHADER_VARIANT_LINE_TUBE_CAP);
 
     // Shader feature flags enum
     nb::enum_<tc_shader_feature>(m, "ShaderFeature")
